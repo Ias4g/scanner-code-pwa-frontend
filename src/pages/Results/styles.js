a0d95c30e8a3387c0e75a9695eb40688c23d7cc7
Izael silva <izaell.oficial@hotmail.com>
2020-12-20 15:04:08 -0300

    Update styles.js

--- src/pages/Results/styles.js
+++ src/pages/Results/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  padding: 20px;
   position: fixed;
   right: 150px;
   bottom: 20px;
@@ -17,8 +18,12 @@ export const Container = styled.div`
   }
 
   @media (min-width: 320px) and (max-width: 480px) {
-    right: 20px;
-    left: 20px;
+    display: flex;
+    align-items: center;
+    top: 100px;
+    right: 30px;
+    left: 30px;
+    bottom: 80px;
 
     a {
       padding: 0px;
@@ -29,7 +34,16 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
     display: flex;
     align-items: center;
+    justify-content: center;
     padding: 10px;
+
+    @media (min-width: 320px) and (max-width: 480px) {
+      background-color: #ff0400;
+      height: 100%;
+      flex-direction: column;
+      align-items: center;
+      padding: 2px;
+    }
 `;
 
 export const Cover = styled.img`
@@ -40,9 +54,11 @@ export const Cover = styled.img`
     border: 1px solid #7f8c8d;
 
     @media (min-width: 320px) and (max-width: 480px) {
-        height: 96px;
-        width: 72px;
-        margin-right: 10px;
+      height: 128px;
+      width: 96px;
+      margin-top: 20px;
+      margin-right: 0px;
+      margin-bottom: 20px;
     }
 `;
 
@@ -81,48 +97,61 @@ export const Info = styled.div`
     .name-author {
       .name {
         color: #7f8c8d;
-        font-size: 14px;
-        margin-bottom: 2px;
+        font-size: 16px;
+        margin-bottom: 5px;
         margin-top: 0;
       }
 
       .author {
         opacity: 0.7;
         color: #7f8c8d;
-        font-size: 12px;
+        font-size: 14px;
         font-style: italic;
-        margin-bottom: 10px;
+        margin-bottom: 15px;
         padding: 0px;
+        text-align: center;
       }
     }
 
     .book-ratings {
         color: #7f8c8d;
         font-size: 12px;
+        text-align: center;
+        margin-bottom: 5px;
     }
 
     .price {
         color: #7f8c8d;
         font-size: 12px;
+        text-align: center;
     }
   }
 `;
 
 export const ActionButton = styled.div`
-    margin-left: 10px;
-    padding: 0px;
+  margin-left: 10px;
+  padding: 0px;
+
+  .button {
+    display: block;
+    background-color: #2ecc71;
+    border-radius: 50%;
+    padding: 20px;
+    line-height: 0;
+  }
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    display: fixed;
+    bottom: 0;
+    margin-left: 0;
+    padding: 2px;
 
     .button {
-        display: block;
-        background-color: #2ecc71;
-        border-radius: 50%;
-        padding: 20px;
-        line-height: 0;
-
-        @media (min-width: 320px) and (max-width: 480px) {
-            padding: 2px;
-            border-radius: 5px;
-        }
+      display: block;
+      background-color: #2ecc71;
+      border-radius: 4px;
+      padding: 2px;
+      line-height: 0;
     }
-
+  }
 `;
